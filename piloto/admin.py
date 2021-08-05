07859ba461d20d9c3bd73ad866378b47aa37f795
Abdiel Vega <abdiel017@gmail.com>
2021-08-05 00:16:16 -0400

    Added admin page for events

---
+++ piloto/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.http.request import HttpRequest
+from .models import Event
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('id', 'recipient', 'action', 'subject')
+    readonly_fields = ('id', 'recipient', 'action', 'subject')
+
+    def has_add_permission(
+        self, request: HttpRequest
+    ) -> bool:
+        return False
